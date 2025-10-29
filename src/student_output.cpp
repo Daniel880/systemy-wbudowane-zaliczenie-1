c83706fe24a00886265b4175af4890558e27fe93
Kacper Wesolowski <kacper@wesolowski.com>
2025-10-29 20:32:57 +0000

    Zaliczenie

---
+++ src/student_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "system_info.h"
+
+
+void print_student_output()
+{
+std::cout << "HelloWordl!" << std::endl;
+std::cout << "Imie i Nazwisko : KacperWesolowski: << std::endl;
+std::cout << "Nazwa Bruncha : << get_brunch_name() <<std::endl;
+std::cout << "IP: " << get_ip_address() <<"," << std::endl;
+std::cout << "machine_ID : << get_machine_id() << std::endl;
+}
