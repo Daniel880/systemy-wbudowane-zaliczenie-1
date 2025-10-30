6b6a0b96a5b6977bdeba92a7d6682fd11ecfbb65
Mateusz Stodolny <mateusz@stodolny.com>
2025-10-30 04:12:49 +0000

    Dodano funkcje wyswietlajaca tekst

---
+++ src/student_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "system_info.h"
+
+void print_student_output()
+{
+std::cout << "Hello World!"<<std::endl;
+std::cout << "Imie i Nazwisko: "<<"MateuszStodolny"<< std::endl;
+std::cout << "Nazwa Brancha: " <<get_branch_name()<< std::endl;
+std::cout << "IP: " <<get_ip_address()<<","<<std::endl;
+std::cout <<"machine_ID: "<<get_machine_id()<<std::endl;
+}
