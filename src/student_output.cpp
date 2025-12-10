614f5349f6dfae4642d09c97b9b8d7856c133292
Maria Szulc <maria@szulc.com>
2025-12-10 16:51:18 +0000

    opis

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -1,6 +1,14 @@
 # include <iostream>
+# include "../include/system_info.h"
 
 void print_student_output(){
+    std::string branch=get_branch_name();
+    std:: string ip = get_ip_address();
+    std::string machine=get_machine_id();
+
     std::cout<<"Hello World!"<<std::endl;
     std::cout<<"Imie i Nazwisko: "<<"MariaSzulc"<<std::endl;
+    std::cout<<"Nazwa Brancha: "<<branch<<std::endl;
+    std::cout<<"IP: "<<ip<<std::endl;
+    std::cout<<"machine_ID"<<machine<<std::endl;
 }
