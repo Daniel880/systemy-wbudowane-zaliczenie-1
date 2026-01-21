53f2a280f2a00a09ebdca2bbdabe47bc109b133e
Michal Kania <michal@kania.com>
2026-01-21 13:49:48 +0000

    Dodano student_output.cpp

---
+++ src/student_output.cpp
@@ -0,0 +1,12 @@
+#include "student_output.h"
+
+#include <iostream>
+#include "system_info.h"
+
+void print_student_output() {
+std::cout << "Hello World!" << std::endl;
+std::cout << "Imie i Nazwisko: MichalKania" << std::endl;
+std::cout << "Nazwa Brancha: " << get_branch_name() << std::endl;
+std::cout << "IP: " << get_ip_address() << std::endl;
+std::cout << "machine_ID: " << get_machine_id() << std::endl;
+}
