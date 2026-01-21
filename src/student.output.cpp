222cb381e16c64d48ac0fbf1c4b9990cd12cba88
Piotr Krzyzaniak <piotr@krzyzaniak.com>
2026-01-21 13:44:42 +0000

    odd

--- src/student.output.cpp
+++ src/student.output.cpp
@@ -1,14 +1,11 @@
-
 #include <system_info.h>
 #include <iostream>
 #include<student_output.h>
 
 void print_student_output(){
-
 	std::cout<<"Hello World!" <<std::endl;
-	std::cout<<"Imie i Naziwsko: " << "PiotrKrzyzaniak" <<std::endl;
+	std::cout<<"Imie i Nazwisko: " << "PiotrKrzyzaniak" <<std::endl;
 	std::cout<<"Nazwa Brancha: "<<get_branch_name() <<std::endl;
 	std::cout<<"IP: " <<get_ip_address() <<","  <<std::endl;
 	std::cout<<"machine_ID: " <<get_machine_id() <<std::endl;
-
 }
