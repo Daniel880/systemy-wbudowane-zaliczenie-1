74ab9dcb7360cad53c849cbe786d42875d620ce9
Kacper Klimczak <Kacper@Klimczak.com>
2026-01-21 14:11:32 +0000

    Dodanie pliku

---
+++ src/student_output.cpp
@@ -0,0 +1,8 @@
+#include "XYZ.h"
+
+void print_student_output() {
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Imie i Nazwisko: " << KacperKlimczak << std::endl;
+	std::cout << "Nazwa Brancha: " << get_branch_name() << std::endl;
+	std::cout << "IP: " << get_ip_adress() << "," << std::endl;
+	std::cout << "machine_ID: " << get_machine_id() << std::endl;
