6327987f603fa1dfb915be1ae715636145b79058
TomaszWalkiewicz <tomasz.walkiewicz@student.put.poznan.pl>
2026-01-21 14:22:55 +0000

    123

--- src/student_output.cpp
+++ src/student_output.cpp
@@ -4,7 +4,7 @@
 void print_student_output() {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Imie i Nazwisko: TomaszWalkiewicz" << std::endl;
-	std::cout << "Nazwa brancha: " << get_branch_name() << std::endl;
+	std::cout << "Nazwa Brancha: " << get_branch_name() << std::endl;
 	std::cout << "IP: " << get_ip_address() << "," << std::endl;
 	std::cout << "machine_ID: " << get_machine_id() << std::endl;
 }
